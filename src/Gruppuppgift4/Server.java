a0b29c0509a756a4448ea4e5aa6a2560632dee9a
Nissem01 <nils.modig02@gmail.com>
2025-11-24 12:14:06 +0100

    Jag tog bort mina tidigare ändringar för att någonting gjorde så att "spelet" inte körde igång, vet inte riktigt varför.

--- src/Gruppuppgift4/Server.java
+++ src/Gruppuppgift4/Server.java
@@ -11,35 +11,29 @@ public class Server {
         try{
             while (true){
                 ServerSidePlayer player1 = new ServerSidePlayer(listener.accept(), '1');
-                player1.sendMessage("Welcome player1, waiting for player2 to connect");
                 ServerSidePlayer player2 = new ServerSidePlayer(listener.accept(), '2');
-                player2.sendMessage("Welcome player2");
-
                 System.out.println("Both players connected!");
-
-                player1.sendMessage("All clients connected!");
-                player2.sendMessage("All clients connected!");
 
                 player1.askQuestion("Vad är 1 + 1?");
                 player2.askQuestion("Vad är 1 + 1?");
 
                 String answer1 = player1.getAnswer();
                 String answer2 = player2.getAnswer();
+
                 System.out.println("player 1 answer: " + answer1);
                 System.out.println("player 2 answer: " + answer2);
 
                 if (answer1.equals("2")){
-                    player1.sendMessage("Rätt svar!");
+                    player1.sendMessage("Rätt svar!!");
                 } else {
                     player1.sendMessage("Fel svar!");
                 }
 
                 if (answer2.equals("2")){
-                    player2.sendMessage("Rätt svar!");
+                    player2.sendMessage("Rätt svar!!");
                 } else {
                     player2.sendMessage("Fel svar!");
                 }
-
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
