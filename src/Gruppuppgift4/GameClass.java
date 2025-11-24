b093f7b10c361eae01e24a2a6433c7205825f61a
Linda Winther <Linda.Winther@outlook.com>
2025-11-24 11:53:21 +0100

    ett test till

--- src/Gruppuppgift4/GameClass.java
+++ src/Gruppuppgift4/GameClass.java
@@ -1,4 +1,104 @@
 package Gruppuppgift4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameClass {
+
+
+    String p1 = "Player1";
+    String p2 = "Player2";
+    List<String> q = new ArrayList<>();
+
+
+    public GameClass() {
+        //    * kategori;fråga;SVAR;Felsvar;Felsvar;Felsvar;
+   // * reader:  delar upp i 5 delar (kategori)(1fråga) (4svar)
+    }
+
+
+    public searchCatogoryFromList {
+        // tar ut djur och natur
+    }
+
+    public searchQuestionsFromList {
+//            väljer ut 3 frågor om d&N
+    }
+
+
+
+
+
+
+    /*
+    *
+    * input namn
+    * anslutning upprättad
+    * fråga klient 1 väntar klient2
+    * sökerKategri()
+    * 10 frågor lista
+    * 3frågor random3 1,5,9 (1)
+    *
+    * SkrivUtFråga()
+    * knapp1 = SVAR
+    * knapp2 = Felsvar
+    * knapp3 = Felsvar
+    * knapp4 = Felsvar
+    * *Collections.shullfe
+    *
+    *
+    *     * kategori;fråga;SVAR;Felsvar;Felsvar;Felsvar;
+    * reader:  delar upp i 5 delar (kategori)(1fråga) (4svar)
+    *alltid svar 1 rätt.
+    * lägger in i en arraylist
+
+    *lägger ut på
+    *
+    *
+    *
+    * package sprint2.BestGymEver;
+
+import java.time.LocalDate;
+
+
+public void loadMemberFile(String file) {//Jag kan inte lämna parantesen tom men den används inte
+        try (BufferedReader br = new BufferedReader(new FileReader("src/sprint2/BestGymEver/Memberfile.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(";");
+                if (parts.length < 7) continue;
+                if (parts[0].equalsIgnoreCase("Namn")) continue; // för att hoppa över första raden med titlar
+
+                String name = parts[0].trim();
+                String address = parts[1].trim();
+                String email = parts[2].trim();
+                String ssn = parts[3].trim();
+                LocalDate firstPaymentDate = LocalDate.parse(parts[4].trim());
+                LocalDate lastPaymentDate = LocalDate.parse(parts[5].trim());
+                String memberLevel = parts[6].trim();
+
+                Member m = new Member(name, address, email, ssn, firstPaymentDate, lastPaymentDate, memberLevel);
+                memberList.add(m);
+            }
+        } catch (Exception e) {
+            //System.out.println("Error reading file");
+            e.printStackTrace();
+        }
+        *
+        *  try (BufferedReader br = new BufferedReader(new FileReader(m.toString()))) {
+            br.readLine();
+
+            String input;
+            while ((input = br.readLine()) != null) {
+                String[] medlem = input.split(";");
+
+                Medlem ny = new Medlem(medlem[0], medlem[1], medlem[2], medlem[3], medlem[4], medlem[5], medlem[6]);
+                list.add(ny);
+            }
+        *
+    * */
+
+
+
+
 }
---
+++ src/Gruppuppgift4/Questions.java
@@ -0,0 +1,19 @@
+package Gruppuppgift4;
+
+public class Questions {
+    String category;
+    String question;
+    String answer;
+    String wrong1;
+    String wrong2;
+    String wrong3;
+
+    public Questions(String category, String question, String answer, String wrong1, String wrong2, String wrong3) {
+        this.category = category;
+        this.question = question;
+        this.answer = answer;
+        this.wrong1 = wrong1;
+        this.wrong2 = wrong2;
+        this.wrong3 = wrong3;
+    }
+}
