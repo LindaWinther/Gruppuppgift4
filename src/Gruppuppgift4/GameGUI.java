2607227b2324fb90bc5ca46562ab8cbf00f72773
Nissem01 <nils.modig02@gmail.com>
2025-11-26 17:59:46 +0100

    Gjorde så att servern skickar en fråga till klienten med hjälp av gameclass. I teorin ska klienten då få frågorna och köras in i GUI:n, men det funkar inte riktigt helt än.

--- src/Gruppuppgift4/GameGUI.java
+++ src/Gruppuppgift4/GameGUI.java
@@ -165,36 +165,32 @@ public class GameGUI extends JFrame {
         }
     }
 
-    // Färgar svaret
-//    private void checkAnswer(int index) {
-//
-//        if (index == correctAnswer) {
-//            answerButtons[index].setBackground(new Color(0, 180, 0)); // Grönt för rätt
-//        } else {
-//            answerButtons[index].setBackground(new Color(180, 0, 0)); // Rött för fel
-//        }
-//        lockAnswerButtons(false);
-//    }
-
+//     Färgar svaret
     private void checkAnswer(int index) {
 
         client.send(answerButtons[index].getText());
+        if (index == correctAnswer) {
+            answerButtons[index].setBackground(new Color(0, 180, 0)); // Grönt för rätt
+        } else {
+            answerButtons[index].setBackground(new Color(180, 0, 0)); // Rött för fel
+        }
+        lockAnswerButtons(false);
     }
 
-    public void receiveFromServer(String msg) {
+
+    public void receiveFromServer(String fromServer) {
         SwingUtilities.invokeLater(() -> {
-            if (msg.startsWith("Fråga:")) {
-                String fråga = msg.substring(6);
-                questionLabel.setText(fråga);
-                cardLayout.show(mainPanel, "QUESTION");
+            if (fromServer.startsWith("Fråga;")) {
+                String[] parts = fromServer.split(";");
+                loadQuestion(parts[1], new String[]{parts[2],parts[3],parts[4],parts[5]});
             }
-            if (msg.equals("Rätt!")) {
+            if (fromServer.equals("Rätt!")) {
                 JOptionPane.showMessageDialog(this, "Rätt!");
             }
-            if (msg.equals("Fel!")) {
+            if (fromServer.equals("Fel!")) {
                 JOptionPane.showMessageDialog(this, "Fel svar!");
             }
-            if (msg.equals("GAME_OVER")) {
+            if (fromServer.equals("GAME_OVER")) {
                 JOptionPane.showMessageDialog(this, "Spelet är slut!");
             }
         });
--- src/Gruppuppgift4/Server.java
+++ src/Gruppuppgift4/Server.java
@@ -3,10 +3,20 @@ package Gruppuppgift4;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class Server {
 
     public static void main(String[] args) throws IOException {
+
+        GameClass game = new GameClass();
+        game.readList();
+        game.searchCategoryFromList();
+        List<Questions> questions = game.searchQuestionsFromList();
+
+        Questions q = questions.get(1);
+
+
         ServerSocket server = new ServerSocket(55555);
         System.out.println("Server is running!");
 
@@ -19,8 +29,8 @@ public class Server {
         s1.start();
         s2.start();
 
-        s1.sendMessage("Fråga: Vad är 1+1?");
-        s2.sendMessage("Fråga: Vad är 1+1?");
+        s1.sendMessage("Fråga;" + q.question + ";" + q.answer + ";" + q.wrong1 + ";" + q.wrong2 + ";" + q.wrong3);
+        s2.sendMessage("Fråga;" + q.question + ";" + q.answer + ";" + q.wrong1 + ";" + q.wrong2 + ";" + q.wrong3);
 
     }
 
